6ac3dd837eb67109a238aaa04646795c6a0dacc2
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-08-24 18:55:48 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,12 +1,7 @@
 # Build your say_hello method here
 
-def say_hello(name="Hello Ruby Programmer!")
-  if name = "Hello Ruby Programmer!"
-    puts name
-  else
-    puts "Hello #{name}!"
-  end
-end
+def say_hello(name)
+  puts n
 
 
 
